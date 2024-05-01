899d1a144b5be0337b3981ae93bbc87e379d989b
Yhawbee1 <antwibrian82@gmail.com>
2024-05-01 20:34:17 +0000

    Add form to the app

--- src/app/page/details/details.component.ts
+++ src/app/page/details/details.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../../services/housing/housing.service';
 import { HousingLocation } from '../../interfaces/housinglocation';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
@@ -16,8 +17,25 @@ export class DetailsComponent {
   housingLocationId = -1;
   housingService: HousingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  })
+
   constructor() {
     this.housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
+
+    
   }
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    )
+  }
+  
 }
