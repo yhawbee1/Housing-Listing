acc76058d31963a42448a8e32d8292d0f5e6a1fc
Yhawbee1 <antwibrian82@gmail.com>
2024-05-01 20:58:17 +0000

    Add search functionality

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -11,11 +11,20 @@ import { HousingService } from '../../services/housing/housing.service';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  
-housingLocationList: HousingLocation[] = []
-  housingService: HousingService = inject(HousingService)
 
+  housingLocationList: HousingLocation[] = []
+  housingService: HousingService = inject(HousingService)
+  filteredLocationList: HousingLocation[] = [];
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+  filteredResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) => housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    );
   }
 }
